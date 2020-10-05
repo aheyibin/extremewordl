fa1b39754cebe60aaa8788bc6a5bb6db047dc632
Oldcolor <362734047@qq.com>
2020-10-06 02:44:26 +0800

    github copy

--- Src/Server/GameServer/GameServer/GameServer.cs
+++ Src/Server/GameServer/GameServer/GameServer.cs
@@ -24,10 +24,9 @@ namespace GameServer
         {
             network = new NetService();
             network.Init(8000);
+            DBService.Instance.Init();
             //HelloWorldService.Instance.Init();
-            //DBService.Instance.Init();
-            //var a = DBService.Instance.Entities.Characters.Where(s => s.TID == 1);
-            //Console.WriteLine("{0}",a.FirstOrDefault<TCharacter>().Name);
+            UserService.Instance.Init();
             thread = new Thread(new ThreadStart(this.Update));
 
             return true;
@@ -37,6 +36,7 @@ namespace GameServer
         {
             network.Start();
             //HelloWorldService.Instance.Start();
+
             running = true;
             thread.Start();
         }
